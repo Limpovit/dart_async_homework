9a4194ca28d53430e2fead663e11a643ddfa113a
Vitalii Lymar <32154871+Limpovit@users.noreply.github.com>
2025-07-12 22:27:47 +0300

    fix(DART-11): replased " to '

--- lib/homeworks/lesson_11/hw_futures.dart
+++ lib/homeworks/lesson_11/hw_futures.dart
@@ -2,10 +2,10 @@ import 'dart:io';
 
 void main(List<String> args) async {
   // Task 1: Асинхронне отримання імені
-  stdout.write("Мене звати ${await fetchName()}");
+  stdout.write('Мене звати ${await fetchName()}');
 
   // Task 2: Асинхронне отримання віку
-  stdout.write("\nМені ${pluralizeYears(await fetchAge())}\n");
+  stdout.write('\nМені ${pluralizeYears(await fetchAge())}\n');
 
   // Task 3: Послідовне виконання Future
 
@@ -19,7 +19,7 @@ void main(List<String> args) async {
   await fetchAge();
   stopwatch.stop();
   print(
-    "Методи виконувались послідовно ${stopwatch.elapsedMilliseconds} мілісекунди",
+    'Методи виконувались послідовно ${stopwatch.elapsedMilliseconds} мілісекунди',
   );
 
   // Task 4: Паралельне виконання Future (Future.wait)
@@ -32,7 +32,7 @@ void main(List<String> args) async {
   await Future.wait([fetchName(), fetchAge()]);
   stopwatch.stop();
   print(
-    "Методи виконувались паралельно ${stopwatch.elapsedMilliseconds} мілісекунди",
+    'Методи виконувались паралельно ${stopwatch.elapsedMilliseconds} мілісекунди',
   );
 
   // Task 5: Зворотний відлік з затримкою
@@ -44,31 +44,31 @@ void main(List<String> args) async {
 
 // Створіть метод Future<String> fetchName(), який імітує асинхронну операцію.
 // Цей метод повинен повертати ваше ім'я через 2 секунди (використайте Future.delayed та async/await).
-// Виведіть результат у консоль у форматі: "Мене звати [результат виконання асинхронної операції]".
+// Виведіть результат у консоль у форматі: 'Мене звати [результат виконання асинхронної операції]'.
 
 Future<String> fetchName() async {
   return await Future.delayed(
     const Duration(seconds: 2),
-    () => "Vitalii Lymar",
+    () => 'Vitalii Lymar',
   );
 }
 
 // Task 2: Асинхронне отримання віку
 
 // Створіть метод Future<String> fetchAge(), який імітує асинхронну операцію.
-// Цей метод повинен повертати рядок "25" через 1500 мілісекунд.
-// Виведіть результат у консоль у форматі: "Мені [ваш вік] років".
-// Реалізуйте логіку, щоб слово "рік" змінювалося відповідно до значення віку (наприклад: 21 рік, 22 роки, 25 років).
+// Цей метод повинен повертати рядок '25' через 1500 мілісекунд.
+// Виведіть результат у консоль у форматі: 'Мені [ваш вік] років'.
+// Реалізуйте логіку, щоб слово 'рік' змінювалося відповідно до значення віку (наприклад: 21 рік, 22 роки, 25 років).
 
 Future<String> fetchAge() async {
-  return await Future.delayed(const Duration(milliseconds: 1500), () => "27");
+  return await Future.delayed(const Duration(milliseconds: 1500), () => '27');
 }
 
 String pluralizeYears(String ageStr) {
-  if (ageStr.isEmpty) return "Невірне значення";
+  if (ageStr.isEmpty) return 'Невірне значення';
 
   int age = int.tryParse(ageStr) ?? -1;
-  if (age < 0) return "Невірне значення";
+  if (age < 0) return 'Невірне значення';
 
   String lastChar = ageStr[ageStr.length - 1];
   String lastTwo = ageStr.length >= 2
@@ -79,31 +79,31 @@ String pluralizeYears(String ageStr) {
       lastTwo == '12' ||
       lastTwo == '13' ||
       lastTwo == '14') {
-    return "$ageStr років";
+    return '$ageStr років';
   }
 
   switch (lastChar) {
     case '1':
-      return "$ageStr рік";
+      return '$ageStr рік';
     case '2':
     case '3':
     case '4':
-      return "$ageStr роки";
+      return '$ageStr роки';
     default:
-      return "$ageStr років";
+      return '$ageStr років';
   }
 }
 
 // Task 5: Зворотний відлік з затримкою
 
 // Напишіть функцію Future<String> delayedCountdown(int seconds), яка приймає кількість секунд.
-// Ця функція повинна щосекунди виводити у консоль відлік (наприклад: "3...", "2...", "1...").
-// В кінці відліку метод має повертати рядок "Старт!".
+// Ця функція повинна щосекунди виводити у консоль відлік (наприклад: '3...', '2...', '1...').
+// В кінці відліку метод має повертати рядок 'Старт!'.
 
 Future<String> delayedCountdown(int seconds) async {
   for (int i = seconds; i > 0; i--) {
-    print("$i...");
+    print('$i...');
     await Future.delayed(const Duration(seconds: 1));
   }
-  return ("Старт!");
+  return ('Старт!');
 }
