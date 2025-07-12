8fc59526088c16c1bc4244cd1b7629ae6d84eab1
Vitalii Lymar <32154871+Limpovit@users.noreply.github.com>
2025-07-12 16:43:21 +0300

    feat(DART-11): solved task 2

--- lib/homeworks/lesson_11/hw_futures.dart
+++ lib/homeworks/lesson_11/hw_futures.dart
@@ -5,6 +5,9 @@ void main(List<String> args) async {
   // Task 1: Асинхронне отримання імені
   stdout.write("Мене звати ${await fetchName()}");
 
+  // Task 2: Асинхронне отримання віку
+  stdout.write("\nМені ${pluralizeYears(await fetchAge())}");
+
 }
 
 // Task 1: Асинхронне отримання імені
@@ -18,4 +21,45 @@ Future<String> fetchName() async {
     const Duration(seconds: 2),
     () => "Vitalii Lymar",
   );
+}
+
+// Task 2: Асинхронне отримання віку
+
+// Створіть метод Future<String> fetchAge(), який імітує асинхронну операцію.
+// Цей метод повинен повертати рядок "25" через 1500 мілісекунд.
+// Виведіть результат у консоль у форматі: "Мені [ваш вік] років".
+// Реалізуйте логіку, щоб слово "рік" змінювалося відповідно до значення віку (наприклад: 21 рік, 22 роки, 25 років).
+
+Future<String> fetchAge() async {
+
+  return await Future.delayed(const Duration(milliseconds: 1500), () => "1");
+
+}
+
+String pluralizeYears(String ageStr) {
+
+ if (ageStr.isEmpty) return "Невірне значення";
+
+  int age = int.tryParse(ageStr) ?? -1;
+  if (age < 0) return "Невірне значення";
+
+  String lastChar = ageStr[ageStr.length - 1];
+  String lastTwo = ageStr.length >= 2
+      ? ageStr.substring(ageStr.length - 2)
+      : lastChar;
+
+  if (lastTwo == '11' || lastTwo == '12' || lastTwo == '13' || lastTwo == '14') {
+    return "$ageStr років";
+  }
+
+  switch (lastChar) {
+    case '1':
+      return "$ageStr рік";
+    case '2':
+    case '3':
+    case '4':
+      return "$ageStr роки";
+    default:
+      return "$ageStr років";
+  }
 }
