fcf58eaf7a4168b8a25b750356d640c35b6bbdc8
Vitalii Lymar <32154871+Limpovit@users.noreply.github.com>
2025-07-12 22:48:11 +0300

    refactor(DART-11): extract methods from main()

--- lib/homeworks/lesson_11/hw_streams.dart
+++ lib/homeworks/lesson_11/hw_streams.dart
@@ -3,6 +3,14 @@ import 'dart:io';
 import 'dart:math';
 
 void main(List<String> args) async {
+  await streamFromIterable();
+
+  await periodicStream();
+
+  await streamController();
+}
+
+Future<void> streamFromIterable() async {
   // Task 6: Стрім з чисел (fromIterable)
 
   // Створіть Stream<int> з довільних чисел (наприклад, від 1 до 5) за допомогою Stream.fromIterable.
@@ -24,47 +32,46 @@ void main(List<String> args) async {
   stream.listen((value) {
     stdout.write('$value ');
   });
+}
 
+Future<void> periodicStream() async {
   //   Task 7: Зворотний відлік зі стріму (periodic)
 
   // Створіть Stream<int> за допомогою Stream.periodic, який генерує числа з затримкою в 1 секунду.
   // Обмежте відлік до 10 чисел за допомогою методу take.
   // Виведіть кожне число у консоль у форматі: "1...", "2...", "3...", ... "10...".
 
-  final Stream streamPeriodic = Stream.periodic(const Duration(milliseconds: 1), (
-    timer,
-  ) {
-    return timer + 1;
-  }).take(10);
+  final Stream streamPeriodic = Stream.periodic(
+    const Duration(milliseconds: 1),
+    (timer) {
+      return timer + 1;
+    },
+  ).take(10);
 
   await for (var value in streamPeriodic) {
     stdout.write('$value...');
   }
-
-  await streamController();
 }
 
 Future<void> streamController() async {
   //   Task 8: Робота з StreamController
-  
+
   // Створіть StreamController<String>.
   // Додайте до цього контролера кілька довільних рядкових значень вручну (наприклад, "Hello", "World", "Dart").
   // Прослухайте цей стрім (через метод listen) і виведіть всі значення у консоль.
   // Закрийте контролер після додавання всіх значень.
-  
+
   stdout.write('\n');
-  
+
   final controller = StreamController<String>();
 
-    controller.stream.listen((event) {
+  controller.stream.listen((event) {
     stdout.write('$event ');
   });
-  
+
   controller.add('Hello');
   controller.add('World');
   controller.add('Dart');
-  
 
-  
   await controller.close();
 }
